fa70beee2e9b1b4a2031897323f70cc6b63e290f
satriaadam <satriaadamm@gmail.com>
2020-09-18 19:53:43 +0700

    Initial Commit

---
+++ android/app/src/main/kotlin/com/satriaadam/aplikasi_toko/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.satriaadam.aplikasi_toko
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utilities/styles.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+final heading1 = TextStyle(
+  color: Colors.black54,
+  fontFamily: 'Heebo',
+  fontWeight: FontWeight.w600,
+  fontSize: 36.0,
+  height: 1.2,
+);
+
+final heading2 = TextStyle(
+  color: Colors.black54,
+  fontFamily: 'Heebo',
+  fontWeight: FontWeight.w400,
+  fontSize: 28.0,
+  height: 1.2,
+);
+
+final largeText = TextStyle(
+  color: Colors.black54,
+  fontFamily: 'Raleway',
+  fontSize: 24.0,
+  height: 1.2,
+);
+
+final mediumText = TextStyle(
+  color: Colors.black54,
+  fontFamily: 'Raleway',
+  fontSize: 20.0,
+  height: 1.5,
+);
+
